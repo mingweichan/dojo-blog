dfa9798cc7a67357a9824349c6aef0d029035fdf
Ming <mingweichan1@gmail.com>
2023-09-20 09:23:07 +0900

    made it actually run again with this weird passing json through function

--- src/Home.js
+++ src/Home.js
@@ -14,15 +14,21 @@ const Home = () => {
 
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json()
+            })
+            .then((data) => {
+                console.log(data)
+            })
         console.log('use effect ran')
         }, [])
 
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="My Blogs!" handleDelete={handleDelete}/>
+            {/* <BlogList blogs={blogs} title="My Blogs!" handleDelete={handleDelete}/>
             <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Marios blogs" handleDelete={handleDelete} />
             <button onClick={() => setName('luigi')}>Change name</button>
-            <p>{name}</p>
+            <p>{name}</p> */}
         </div>     
      );
 }
