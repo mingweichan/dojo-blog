602b217c9adc7d3efadf584671802df60d009453
Ming <mingweichan1@gmail.com>
2023-09-22 15:51:23 +0900

    Set timeout for loading indicator, where the load fetches the data and throws errors if applicable. Create custom hook "useFetch" that takes in a URL and spits out data, loading state and any loading errors

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
-  const link = "https://www.google.com"
   return (
     <div className="App">
       <Navbar />
-      
       <div className="content">
       <Home />
-        <a href={link}>Google</a>
       </div>
     </div>
   );
--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,15 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {    
-    
-    const [blogs, setBlogs] = useState(null);
-
-    const [name, setName] = useState('mario')
-
-    useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json()
-            })
-            .then(data => {
-                setBlogs(data)
-            })
-        console.log('use effect ran')
-        }, [])
+    const {data: blogs, isLoading, error} = useFetch('http://localhost:8000/blogs')
 
     return ( 
         <div className="home">
+            {error && <div>{error}</div>}
+            {isLoading && <div>Loading...</div> }
             {blogs && <BlogList blogs={blogs} title={blogs.title}/>}
-            <button onClick={() => setName('luigi')}>Change name</button>
-            <p>{name}</p>
         </div>
      );
 }
